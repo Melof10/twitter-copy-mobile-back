e5e3380b635fff44604f9c451ae63e28a01f195e
Melof10 <95fedemelo@gmail.com>
2021-06-11 09:30:43 -0300

    Added query for get all publication

--- querys/publications.js
+++ querys/publications.js
@@ -4,3 +4,7 @@ const Publication = mongoose.model('Publication');
 exports.create = async(data) => {
     return await Publication.create(data);
 }
+
+exports.getAll = async() => {
+    return await Publication.find();
+}
