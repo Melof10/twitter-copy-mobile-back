f5932eb1d227a9681b3ddeab561125c4ca1d2baa
Melof10 <95fedemelo@gmail.com>
2021-06-11 09:58:51 -0300

    Added controller for get one publication

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -1,4 +1,4 @@
-const { create, getAll } = require('../querys/publications');
+const { create, getAll, getOne } = require('../querys/publications');
 const { 
     responseSuccess, 
     responseErrorClient, 
@@ -18,6 +18,15 @@ exports.getPublications = async(req, res) => {
     try {
         const publications = await getAll();
         publications ? responseSuccess(res, publications) : responseErrorClient(res);
+    } catch (error) {
+        responseErrorServer(res, error);
+    }
+}
+
+exports.getPubliacation = async(req, res) => {
+    try {
+        const publication = await getOne(req,params.id);
+        publication ? responseSuccess(res, publication) : responseErrorClient(res);
     } catch (error) {
         responseErrorServer(res, error);
     }
